866c71e88a7a609d28556e0a1d7a285195b45bbe
Yuta Sugiyama <y.sugiyama54@gmail.com>
2023-05-06 16:56:58 +0900

    Usersテーブルの作成

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo-go-app/config"
+	"todo-go-app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	//fmt.Println(config.Config.Port)
+	//fmt.Println(config.Config.SQLDriver)
+	//fmt.Println(config.Config.DbName)
+	//fmt.Println(config.Config.LogFile)
+	//
+	//log.Println("test")
 
-	log.Println("test")
+	fmt.Println(models.Db)
 }
