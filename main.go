658437e9b588bf3c14036792347257f67834f7be
Yuta Sugiyama <y.sugiyama54@gmail.com>
2023-05-13 12:15:15 +0900

    サーバーの設定

--- main.go
+++ main.go
@@ -2,44 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"todo-go-app/app/controllers"
 	"todo-go-app/app/models"
 )
 
 func main() {
-	//fmt.Println(config.Config.Port)
-	//fmt.Println(config.Config.SQLDriver)
-	//fmt.Println(config.Config.DbName)
-	//fmt.Println(config.Config.LogFile)
-	//
-	//log.Println("test")
-
 	fmt.Println(models.Db)
 
-	//u := &models.User{}
-	//u.Name = "name"
-	//u.Email = "test2@test.com"
-	//u.PassWord = "password"
-	//u.CreateUser()
-	//
-	//user, _ := models.GetUser(2)
-	//user.CreateTodo("this is content.")
-
-	//t, _ := models.GetTodo(1)
-	//fmt.Println(t)
-
-	//user, _ := models.GetUser(2)
-	//user.CreateTodo("this is second content.")
-
-	//todos, _ := models.GetTodos()
-	//fmt.Println(todos)
-
-	//user, _ := models.GetUser(2)
-
-	t, _ := models.GetTodo(1)
-	//t.Content = "updated2"
-	//t.UserID = 3
-	//t.UpdateTodo()
-
-	t.DeleteTodo()
-
+	err := controllers.StartMainServer()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
