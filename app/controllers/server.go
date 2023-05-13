ddbc989400fb2a71026ab345c8a051beed3ca7cc
Yuta Sugiyama <y.sugiyama54@gmail.com>
2023-05-13 16:02:06 +0900

    layoutの共通化

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"todo-go-app/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+	tamplates := template.Must(template.ParseFiles(files...))
+	tamplates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
